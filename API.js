9c7dfea061b651bc6c433f087246e1fe82ee94b6
Vilius Eidukonis <viliuseidukonis@gmail.com>
2021-11-15 19:28:00 +0200

    4: API

---
+++ API.js
@@ -0,0 +1,27 @@
+const baseURL = 'http://localhost:3000';
+
+class API {
+    static fetchApartments = (success, failure) => {
+        fetch(`${baseURL}/apartments`)
+        .then(res => res.json())
+        .then(success)
+        .catch(failure)
+    }
+
+    static deleteApartment = (id, success, failure) => {
+        fetch(`${baseURL}/apartments/${id}`,{method: 'DELETE'} )
+        .then(res => res.ok ? success() : failure(res.statusText))
+        .catch(failure)
+    }
+}
+
+//API.fetchApartments(
+//    console.log,
+//    console.error
+//)
+
+//API.deleteApartment(
+//    "4",
+//    () => console.log('Ištrinta'),
+//    console.error
+//)
